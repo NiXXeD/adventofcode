129c969e32147c2d9378a1a5f932732d139208cd
Andy Christianson <nix@nixxed.com>
2015-12-02 00:39:13 -0600

    Day 2

---
+++ day2.js
@@ -0,0 +1,6 @@
+var dims = require('fs').readFileSync('input', 'utf-8').trimRight()
+    .split('\n').map(i => i.split('x').map(i => +i).sort((a, b) => a - b))
+console.log(dims.map(d => {
+    return {a: (3 * d[0] * d[1]) + (2 * d[1] * d[2]) + (2 * d[2] * d[0]),
+            b: (2 * d[0]) + (2 * d[1]) + (d[0] * d[1] * d[2])}
+}).reduce((r, v) => ({a:r.a + v.a,b:r.b + v.b}), {a:0, b:0}))
