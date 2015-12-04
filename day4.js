dcb0c3bcb6b0f8010f447bf817e3cbca7af31574
Andy Christianson <nix@nixxed.com>
2015-12-04 10:12:30 -0600

    Shorter, parameterized Day4.

--- day4.js
+++ day4.js
@@ -1,2 +1,1 @@
-var x=1; while(x++)if(require('md5')('bgvyzdsv'+x).startsWith('000000'))break;console.log(x)
-
+var x=1;while(!require('md5')(process.argv[2]+x++).startsWith('000000'));console.log(x)
