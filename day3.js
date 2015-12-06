bebdf24db9bdfc3fe050c322e8106045516fd72d
Andy Christianson <nix@nixxed.com>
2015-12-06 02:31:02 -0600

    More terse

--- day3.js
+++ day3.js
@@ -1,11 +1,7 @@
-var s = {x:0,y:0}, b = {x:0,y:0}
-console.log(Object.keys(Array.from(process.argv[2]).reduce((r, v, i) => {
-    return (w => {
-        if (v == '^') w.y++
-        if (v == '>') w.x++
-        if (v == 'v') w.y--
-        if (v == '<') w.x--
-        r[`${w.x},${w.y}`] = ++r[`${w.x},${w.y}`] || 1
-        return r
-    })(i % 2 ? s : b)
-}, {'0,0': 2})).length)
+var s = {x: 0, y: 0}, b = {x: 0, y: 0}
+console.log(Object.keys(Array.from(process.argv[2]).reduce((r, v, i) =>(w => {
+    w.x += {'>': 1, '<': -1}[v] || 0
+    w.y += +{'^': 1, 'v': -1}[v] || 0
+    r[`${w.x},${w.y}`] = 1
+    return r
+})(i % 2 ? s : b), {'0,0': 1})).length)
