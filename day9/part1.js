7b7b513fbf7861c41adf7bb36caa4bb732e68928
Andy Christianson <nix@nixxed.com>
2015-12-09 11:34:45 -0600

    Day 9

---
+++ day9/part1.js
@@ -0,0 +1,6 @@
+module.exports = (i, m='min') => {
+    var _ = require('lodash')
+    var z = i.map(i => i.match(/(\S+) to (\S+) = (\d+)/)).map(i => ({a: [i[1], i[2]], d: i[3]}))
+    var x = require('js-combinatorics').permutation(_(z).map('a').flatten().uniq().value())
+    return _[m](x.map(t => t.reduce((r, v) => ({p: v, d: r.p ? +_.find(z, {a: [r.p, v]}).d + r.d: 0}), {}).d))
+}
---
+++ day9/part2.js
@@ -0,0 +1,1 @@
+module.exports = i => require('./part1')(i, 'max')
