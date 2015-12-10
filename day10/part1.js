1917901349a1b8e6c78ab0ec45fa29b287333737
Andy Christianson <nix@nixxed.com>
2015-12-10 10:02:20 -0600

    More terse

--- day10/part1.js
+++ day10/part1.js
@@ -1,3 +1,1 @@
-module.exports = (i, t = 40) => (function f(s, x) {
-    return x++ < t ? f(s.match(/(\d)\1*/g).reduce((r, v) => r + v.length + v[0], ''), x) : s
-})(i, 0).length
+module.exports = (i, t=40) => [...Array(t)].reduce(r => r.match(/(.)\1*/g).map(v => v.length + v[0]).join``, i).length
