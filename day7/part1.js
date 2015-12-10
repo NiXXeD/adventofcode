1bd692f5931c9505e38bf4c1669be827cd21df73
Andy Christianson <nix@nixxed.com>
2015-12-10 14:41:06 -0600

    More for less!

--- day7/part1.js
+++ day7/part1.js
@@ -1,15 +1,11 @@
-module.exports = (input, wires={}) => {
-    var test = i => !i || wires.hasOwnProperty(i) || /\d+/.test(i)
-    var val = i => wires[i] || +i
+module.exports = (i, w={}) => {
     var ops = {AND: (a, b) => a & b, OR: (a, b) => a | b, LSHIFT: (a, b) => a << b,
         RSHIFT: (a, b) => a >> b, NOT: (a, b) => b ^ 65535, VAL: (a, b) => b}
-
-    while (input.length) {
-        var [o, a, op, b, c] = input.shift().match(/([a-z0-9]*)\b\s?([A-Z]+)?\s?(\S+)\s->\s(\S+)/)
-        if (!test(c))
-            if (test(a) && test(b)) wires[c] = ops[op || 'VAL'](val(a), val(b))
-            else input.push(o)
+    while (i.length) {
+        var [o, a, op, b, c] = i.shift().match(/([a-z0-9]*)\b\s?([A-Z]+)?\s?(\S+)\s->\s(\S+)/)
+        if ([a, b].every(i => !i || w.hasOwnProperty(i) || /\d+/.test(i)))
+            w[c] = w[c] || ops[op || 'VAL'](...[a, b].map(i => w[i] || +i))
+        else i.push(o)
     }
-
-    return wires.a
+    return w.a
 }
