34f1202510d607b5428d5f856211466b4a921825
Andy Christianson <nix@nixxed.com>
2015-12-11 01:40:27 -0600

    Much shorter and faster.

--- day11/part1.js
+++ day11/part1.js
@@ -1,19 +1,5 @@
-module.exports = input => {
-    var toNum = i => i.split``.reduce((r, v, i) => r + Math.pow(26, 7 - i) * (v.charCodeAt(0) - 97), 0)
-    var toStr = n => [...Array(8)].reduce((r, v, i) => {
-        var p = Math.pow(26, 7 - i)
-        var m = r.n % p
-        var d = (r.n - m) / p
-        r.n = m
-        r.s += String.fromCharCode(d + 97)
-        return r
-    }, {s: '', n: n}).s
-
-    var q = [...Array(24).keys()].map(i => [i + 97, i + 98, i + 99].map(c => String.fromCharCode(c)).join``)
-    var test = s => require('lodash').uniq(s.match(/(.)\1/g) || []).length > 1 && q.some(p => ~s.indexOf(p))
-
-    do input = toStr(toNum(input) + 1).replace('i', 'j').replace('l', 'm').replace('o', 'p')
-    while (!test(input))
-
-    return input
+module.exports = i => {
+    do i = (parseInt(i, 36) + 1).toString(36).replace(/0/, 'a').replace(/i/, 'j').replace(/l/, 'm').replace(/o/, 'p')
+    while (!/(.)\1.*(.)\2/.test(i) || !/(abc|bcd|cde|def|efg|fgh|pqr|qrs|rst|stu|tuv|uvw|vwx|wxy|xyz)/.test(i))
+    return i
 }
