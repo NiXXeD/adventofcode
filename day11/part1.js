f8ffcd519d1086bdd0f14a671cb7f9cd8a177b61
Andy Christianson <nix@nixxed.com>
2015-12-12 14:44:58 -0600

    Fix regex

--- day11/part1.js
+++ day11/part1.js
@@ -1,5 +1,5 @@
 module.exports = i => {
     do i = (parseInt(i, 36) + 1).toString(36).replace(/0/, 'a').replace(/i/, 'j').replace(/l/, 'm').replace(/o/, 'p')
-    while (!/(.)\1.*(.)\2/.test(i) || !/(abc|bcd|cde|def|efg|fgh|pqr|qrs|rst|stu|tuv|uvw|vwx|wxy|xyz)/.test(i))
+    while (!/(.)\1.*(?!\1)(.)\2/.test(i) || !/(abc|bcd|cde|def|efg|fgh|pqr|qrs|rst|stu|tuv|uvw|vwx|wxy|xyz)/.test(i))
     return i
 }
