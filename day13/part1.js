05024395fed4024bd00942f9fefae4c171e40216
Andy Christianson <nix@nixxed.com>
2015-12-13 13:11:51 -0600

    More for less!

--- day13/part1.js
+++ day13/part1.js
@@ -1,10 +1,10 @@
-module.exports = i => {
-    i = i.map(s => s.match(/^(\w+).+?(\w+)\s(\d+).+?(\w+).$/)).reduce((r, v) => {
-        r.keys[v[1]] = 1
-        r[`${v[1]},${v[4]}`] = v[2] === 'lose' ? 0 - +v[3] : +v[3]
+module.exports = (i, d={}) => {
+    i = i.map(s => s.match(/^(\w+).+?(\w)\s(\d+).+?(\w+).$/)).reduce((r, v) => {
+        r.k[v[1]] = 1
+        r[`${v[1]},${v[4]}`] = +v[3] * {e: -1, n: 1}[v[2]]
         return r
-    }, {keys: {}})
-    return require('js-combinatorics').permutation(Object.keys(i.keys)).map(a =>
+    }, {k: d})
+    return require('js-combinatorics').permutation(Object.keys(i.k)).map(a =>
         a.map((p, k, a) => (i[`${p},${a[k - 1] || a[a.length - 1]}`] || 0) + (i[`${p},${a[k + 1] || a[0]}`] || 0))
             .reduce((r, v) => r + v, 0)).reduce((r, v) => r > v ? r : v, 0)
 }
--- day13/part2.js
+++ day13/part2.js
@@ -1,10 +1,1 @@
-module.exports = i => {
-    i = i.map(s => s.match(/^(\w+).+?(\w+)\s(\d+).+?(\w+).$/)).reduce((r, v) => {
-        r.keys[v[1]] = 1
-        r[`${v[1]},${v[4]}`] = v[2] === 'lose' ? 0 - +v[3] : +v[3]
-        return r
-    }, {keys: {Me: 1}})
-    return require('js-combinatorics').permutation(Object.keys(i.keys)).map(a =>
-        a.map((p, k, a) => (i[`${p},${a[k - 1] || a[a.length - 1]}`] || 0) + (i[`${p},${a[k + 1] || a[0]}`] || 0))
-            .reduce((r, v) => r + v, 0)).reduce((r, v) => r > v ? r : v, 0)
-}
+module.exports = i => require('./part1')(i, {m: 1})
