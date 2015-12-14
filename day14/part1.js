a9dfc72cb111009add3806a6cc94f0fa9e08193c
Andy Christianson <nix@nixxed.com>
2015-12-13 23:20:38 -0600

    Day 14

---
+++ day14/part1.js
@@ -0,0 +1,29 @@
+module.exports = input => {
+    var r = input.map(s => s.match(/(\w+).+?(\d+).+?(\d+).+?(\d+)/)).map(m => ({
+        name: m[1],
+        speed: +m[2],
+        flyTime: +m[3],
+        restTime: +m[4],
+        mode: 'f',
+        distance: 0,
+        remaining: +m[3]
+    }))
+
+    for(var d=0; d<2503; d++) {
+        r = r.map(e => {
+            if (e.mode === 'f') {
+                e.distance += e.speed
+            }
+            e.remaining--
+            if (!e.remaining) {
+                e.mode = e.mode === 'f' ? 'r' : 'f'
+                e.remaining = e.mode === 'f' ? e.flyTime : e.restTime
+            }
+            return e
+        })
+
+
+    }
+
+    return r.reduce((r, v) => r > v.distance ? r : v.distance, 0)
+}
