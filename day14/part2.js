41c0b2ff335415a3d5c84e7669205ea2e42c0459
Andy Christianson <nix@nixxed.com>
2015-12-13 23:37:28 -0600

    More terse

--- day14/part2.js
+++ day14/part2.js
@@ -1,40 +1,19 @@
-var _ = require('lodash')
-
 module.exports = input => {
-    var r = input.map(s => s.match(/(\w+).+?(\d+).+?(\d+).+?(\d+)/)).map(m => ({
-        name: m[1],
-        speed: +m[2],
-        flyTime: +m[3],
-        restTime: +m[4],
-        mode: 'f',
-        distance: 0,
-        remaining: +m[3],
-        points: 0
+    var r = input.map(s => s.match(/.+?(\d+).+?(\d+).+?(\d+)/)).map(m => ({
+        speed: +m[1], flyTime: +m[2], restTime: +m[3], distance: 0, remaining: +m[2], points: 0
     }))
 
-    for(var d=0; d<2503; d++) {
-        r = r.map(e => {
-            if (e.mode === 'f') {
-                e.distance += e.speed
-            }
-            e.remaining--
-            if (!e.remaining) {
-                e.mode = e.mode === 'f' ? 'r' : 'f'
+    for (var d = 0; d < 2503; d++) {
+        r.forEach(e => {
+            if (e.mode !== 'r') e.distance += e.speed
+            if (!--e.remaining) {
+                e.mode = e.mode !== 'r' ? 'r' : 'f'
                 e.remaining = e.mode === 'f' ? e.flyTime : e.restTime
             }
-            return e
         })
 
-        var max = 0
-        r.forEach(e => {
-            max = max > e.distance ? max : e.distance
-        })
-
-        var winners = _.filter(r, {distance: max})
-        winners.map(w => {
-            w.points++
-            return w
-        })
+        var max = r.reduce((r, v) => r > v.distance ? r : v.distance, 0)
+        r.filter(e => e.distance == max).forEach(e => e.points++)
     }
 
     return r.reduce((r, v) => r > v.points ? r : v.points, 0)
