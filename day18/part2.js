0ad919ae124c200d94312778acf5b91c148b228a
Andy Christianson <nix@nixxed.com>
2015-12-19 00:20:25 -0600

    Slightly shorter.

--- day18/part2.js
+++ day18/part2.js
@@ -4,10 +4,7 @@ module.exports = input => {
     var neighbors = (g, x, y) => c.reduce((r, a) => r + c.reduce((r, b) => r + ((g[x+a] || {})[y+b] || 0), 0), 0) - g[x][y]
     var iterate = r => r.map((a, x) => r.map((b, y) => +!!~t[r[x][y]].indexOf(neighbors(r, x, y))))
     var fix = r => {
-        r[0][0] = 1
-        r[0][99] = 1
-        r[99][0] = 1
-        r[99][99] = 1
+        r[0][0] = r[0][99] = r[99][0] = r[99][99] = 1
         return r
     }
     return [...Array(100)].reduce(r => fix(iterate(r)), fix(grid)).reduce((r, v) => r + v.reduce((r, v) => r + v, 0), 0)
