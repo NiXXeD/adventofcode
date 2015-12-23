67438f6818742582e39a2530022444718daa7a5d
Andy Christianson <nix@nixxed.com>
2015-12-23 01:29:36 -0600

    More day 23 stuff

--- day23/part1.js
+++ day23/part1.js
@@ -1,15 +1,15 @@
-module.exports = (input, a=0) => {
-    var i = -1, reg = {a: a, b: 0}, program = input.map(s => s.match(/([^, ]+)/g)), ops = {
-        hlf: x => reg[x] = Math.floor(reg[x] / 2),
+module.exports = (input, a = 0) => {
+    let i = 0, reg = {a: a, b: 0}, program = input.map(s => s.match(/([^, ]+)/g)), ops = {
+        hlf: x => reg[x] >>= 1,
         tpl: x => reg[x] *= 3,
         inc: x => reg[x]++,
         jmp: x => i += +x - 1,
         jie: (x, y) => i += reg[x] % 2 === 0 ? +y - 1 : 0,
         jio: (x, y) => i += reg[x] === 1 ? +y - 1 : 0
     }
-    while (++i < program.length) {
-        var op = program[i]
-        ops[op[0]](op[1], op[2])
-    }
+    do {
+        let [o, x, y] = program[i]
+        ops[o](x, y)
+    } while (++i < program.length)
     return reg.b
 }
