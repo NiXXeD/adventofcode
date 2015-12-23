d0e7f3e5468d729573d25fd2cd6b3fae076a24d9
Andy Christianson <nix@nixxed.com>
2015-12-22 22:21:05 -0600

    Shorter day 21

--- day21/part1.js
+++ day21/part1.js
@@ -1,12 +1,11 @@
-module.exports = input => {
-    var store = require('./store')
-    var boss = input.join``.match(/\d+/g)
-
-    return require('lodash').flattenDeep(store.weapons.map(w => store.armor.map(a => store.rings.map(r1 =>
-        store.rings.map(r2 => ({
-            cost: w.cost + a.cost + r1.cost + r2.cost,
-            dmg: w.dmg + r1.dmg + r2.dmg,
-            ar: a.ar + r1.ar + r2.ar
-        })))))).filter(a => 100 / Math.max(1, boss[1] - a.ar) >> 0 >= boss[0] / Math.max(1, a.dmg - boss[2]) >> 0)
-        .reduce((r, v) => v.cost < r ? v.cost : r, 999)
+module.exports = (input, win = 999) => {
+    var store = require('./store'), boss = input.join``.match(/\d+/g), _ = require('lodash')
+    return _.flattenDeep(store.weapons.map(w => store.armor.map(a => store.rings.map(r1 =>
+        store.rings.map(r2 => ({w, a, r1, r2})))))).filter(s => s.r1.id !== s.r2.id)
+        .map(s => ({
+            cost: s.w.cost + s.a.cost + s.r1.cost + s.r2.cost,
+            win: 100 / Math.max(1, boss[1] - (s.a.ar + s.r1.ar + s.r2.ar)) >> 0 >=
+            boss[0] / Math.max(1, (s.w.dmg + s.r1.dmg + s.r2.dmg) - boss[2]) >> 0
+        })).filter(a => a.win === !!win)
+        .reduce((r, v) => Math[win ? 'min' : 'max'](v.cost, r), win)
 }
