1724f3d907e97ee0c2b107bd0a027be96986a337
Andy Christianson <nix@nixxed.com>
2015-12-26 00:47:28 -0600

    Better day 25

--- day25/part1.js
+++ day25/part1.js
@@ -1,18 +1,14 @@
 module.exports = input => {
-    var [sr, sc] = input.match(/(\d+)/g).map(Number).map(n => n - 1)
-    var row = 1, col = 0, i = 20151125, grid = [[i]]
-    while (!grid[sr] || !grid[sr][sc]) {
-        if (!grid[row]) grid.push([])
-        i = (i * 252533) % 33554393
-        grid[row].push(i)
-
-        if (row === 0) {
-            row = grid.length
-            col = 0
+    var [sr, sc] = input.match(/(\d+)/g).map(Number), i = 1, row = 1, col = 1, v = 20151125
+    do {
+        if (row === 1) {
+            row = ++i
+            col = 1
         } else {
             row--
             col++
         }
-    }
-    return grid[sr][sc]
+        v = (v * 252533) % 33554393
+    } while (row !== sr || col !== sc)
+    return v
 }
