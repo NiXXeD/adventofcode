287b491503cf254315a0b30b6d918e92cb1b558f
Andy Christianson <nix@nixxed.com>
2016-12-01 00:15:38 -0600

    Smaller day 1

--- 2016/day1/part1.js
+++ 2016/day1/part1.js
@@ -2,9 +2,7 @@ module.exports = input => {
     let pos = input.split(', ')
         .map(s => ({dir: s[0] === 'R' ? 1 : -1, steps: +s.slice(1)}))
         .reduce((p, v) => {
-            p.f = (p.f + v.dir)
-            if (p.f < 0) p.f = 3
-            if (p.f > 3) p.f = 0
+            p.f = (p.f + v.dir + 4) % 4
             p.x += {0: 0, 1: 1, 2: 0, 3: -1}[p.f] * v.steps
             p.y += {0: 1, 1: 0, 2: -1, 3: 0}[p.f] * v.steps
             return p
