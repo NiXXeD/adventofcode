6d44b780fcd58e2a7dfde3ae20b8ead6e286cbca
Andy Christianson <nix@nixxed.com>
2016-12-01 01:07:27 -0600

    Shorter is better

--- 2016/day1/part1.js
+++ 2016/day1/part1.js
@@ -1,11 +1,7 @@
-module.exports = input => {
-    let pos = input.split(', ')
-        .map(s => ({dir: s[0] === 'R' ? 1 : -1, steps: +s.slice(1)}))
-        .reduce((p, v) => {
-            p.f = (p.f + v.dir + 4) % 4
-            p.x += {0: 0, 1: 1, 2: 0, 3: -1}[p.f] * v.steps
-            p.y += {0: 1, 1: 0, 2: -1, 3: 0}[p.f] * v.steps
-            return p
-        }, {x: 0, y: 0, f: 0})
-    return Math.abs(pos.x) + Math.abs(pos.y)
-}
+module.exports = i => i.split(', ')
+    .map(s => ({r: {R: 1, L: -1}[s[0]], s: +s.slice(1)}))
+    .reduce((p, v) => {
+        let f = (p[0] + v.r + 4) % 4
+        return [f, p[1] + {0: 0, 1: 1, 2: 0, 3: -1}[f] * v.s, p[2] + {0: 1, 1: 0, 2: -1, 3: 0}[f] * v.s]
+    }, [0, 0, 0])
+    .reduce((p, v, i) => !!i ? p + Math.abs(v) : 0, 0)
