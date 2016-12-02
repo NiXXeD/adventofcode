1e75c793a1e0d5e83ddfdb776da7b9681a4a48c1
Andy Christianson <nix@nixxed.com>
2016-12-01 23:29:45 -0600

    Shorter day 2.

--- 2016/day2/part1.js
+++ 2016/day2/part1.js
@@ -7,24 +7,17 @@ module.exports = input => {
 
     let prev = [1, 1]
     return input
-        .map(line => {
-            return line.split``.reduce((p, v) => {
+        .map(line => line.split``
+            .reduce((p, v) => {
                 let n = [p[0], p[1]]
-                if (v === 'U' && p[0] > 0) {
-                    n[0] = p[0] - 1
-                } else if (v === 'D' && p[0] < 2) {
-                    n[0] = p[0] + 1
-                } else if (v === 'L' && p[1] > 0) {
-                    n[1] = p[1] - 1
-                } else if (v === 'R' && p[1] < 2) {
-                    n[1] = p[1] + 1
-                }
+                if (v === 'U') n[0] = p[0] - 1
+                else if (v === 'D') n[0] = p[0] + 1
+                else if (v === 'L') n[1] = p[1] - 1
+                else if (v === 'R') n[1] = p[1] + 1
+                if (!keypad[n[0]] || !keypad[n[0]][n[1]]) n = p
                 prev = n
                 return n
-            }, prev)
-        })
-        .map(coords => {
-            return keypad[coords[0]][coords[1]]
-        })
+            }, prev))
+        .map(coords => keypad[coords[0]][coords[1]])
         .join``
 }
--- 2016/day2/part2.js
+++ 2016/day2/part2.js
@@ -1,11 +1,10 @@
-const _ = require('lodash')
 module.exports = input => {
         let keypad = [
-            [null, null, 1, null, null],
-            [null, 2, 3, 4, null],
+            [0, 0, 1, 0, 0],
+            [0, 2, 3, 4, 0],
             [5, 6, 7, 8, 9],
-            [null, 'A', 'B', 'C', null],
-            [null, null, 'D', null, null]
+            [0, 'A', 'B', 'C', 0],
+            [0, 0, 'D', 0, 0]
         ]
 
         let prev = [2, 0]
@@ -13,22 +12,15 @@ module.exports = input => {
             .map(line => {
                 return line.split``.reduce((p, v) => {
                     let n = [p[0], p[1]]
-                    if (v === 'U' && p[0] > 0) {
-                        n[0] = p[0] - 1
-                    } else if (v === 'D' && p[0] < keypad.length) {
-                        n[0] = p[0] + 1
-                    } else if (v === 'L' && p[1] > 0) {
-                        n[1] = p[1] - 1
-                    } else if (v === 'R' && p[1] < keypad[p[0]].length) {
-                        n[1] = p[1] + 1
-                    }
+                    if (v === 'U') n[0] = p[0] - 1
+                    else if (v === 'D') n[0] = p[0] + 1
+                    else if (v === 'L') n[1] = p[1] - 1
+                    else if (v === 'R') n[1] = p[1] + 1
                     if (!keypad[n[0]] || !keypad[n[0]][n[1]]) n = p
                     prev = n
                     return n
                 }, prev)
             })
-            .map(coords => {
-                return keypad[coords[0]][coords[1]]
-            })
+            .map(coords => keypad[coords[0]][coords[1]])
             .join``
     }
