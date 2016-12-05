f74ed034ceb17ba432c1000ac4aa3712ef82bd02
Andy Christianson <nix@nixxed.com>
2016-12-05 01:04:55 -0600

    Shorter day 4 again.

--- 2016/day4/part1.js
+++ 2016/day4/part1.js
@@ -1,17 +1,14 @@
-const _ = require('lodash')
-module.exports = input => {
-    return input
-        .map(s => s.match(/(.+?)-(\d+)\[(.+?)]/))
-        .map(v => ({
-            sector: +v[2],
-            checksum: v[3].split``,
-            chars: _(_.countBy(v[1].replace(/-/g, '').split``))
-                .map((n, c) => ({c, n}))
-                .orderBy(['n', 'c'], ['desc', 'asc'])
-                .take(5)
-                .map('c')
-                .value()
-        }))
-        .filter(v => v.checksum.every(l => v.chars.includes(l)))
-        .reduce((p, v) => p + v.sector, 0)
-}
+module.exports = i => i.map(s => s.match(/(.+?)-(\d+)\[(.+?)]/))
+    .map(v => ({
+        sector: +v[2],
+        checksum: v[3].split``,
+        chars: require('lodash')(v[1].replace(/-/g, ''))
+            .countBy()
+            .map((n, c) => ({c, n}))
+            .orderBy(['n', 'c'], ['desc', 'asc'])
+            .take(5)
+            .map('c')
+            .value()
+    }))
+    .filter(v => v.checksum.every(l => v.chars.includes(l)))
+    .reduce((p, v) => p + v.sector, 0)
--- 2016/day4/part2.js
+++ 2016/day4/part2.js
@@ -1,8 +1,4 @@
-const _ = require('lodash')
-module.exports = input => {
-    return input
-        .map(s => s.match(/(.+?)-(\d+)\[(.+?)]/))
-        .map(v => ({name: v[1].replace(/-/g, '').split``, sector: +v[2]}))
-        .map(v => _.merge(v, {real: v.name.map(l => String.fromCharCode((((l.charCodeAt(0) - 97) + v.sector) % 26) + 97)).join``}))
-        .find(v => v.real === 'northpoleobjectstorage').sector
-}
+module.exports = i => i.map(s => s.match(/(.+?)-(\d+)\[(.+?)]/))
+    .map(v => ({name: v[1].replace(/-/g, '').split``, sector: +v[2]}))
+    .map(v => Object.assign(v, {real: v.name.map(l => String.fromCharCode((((l.charCodeAt(0) - 97) + v.sector) % 26) + 97)).join``}))
+    .find(v => v.real === 'northpoleobjectstorage').sector
