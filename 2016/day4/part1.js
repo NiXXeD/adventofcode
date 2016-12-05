f4ef62740d30aa4d900ded150d2a476972539959
Andy Christianson <nix@nixxed.com>
2016-12-05 10:44:15 -0600

    Shorter day4 part1

--- 2016/day4/part1.js
+++ 2016/day4/part1.js
@@ -1,14 +1,14 @@
 module.exports = i => i.map(s => s.match(/(.+?)-(\d+)\[(.+?)]/))
     .map(v => ({
         sector: +v[2],
-        checksum: v[3].split``,
+        checksum: v[3],
         chars: require('lodash')(v[1].replace(/-/g, ''))
             .countBy()
             .map((n, c) => ({c, n}))
             .orderBy(['n', 'c'], ['desc', 'asc'])
             .take(5)
             .map('c')
-            .value()
+            .join``
     }))
-    .filter(v => v.checksum.every(l => v.chars.includes(l)))
+    .filter(v => v.checksum === v.chars)
     .reduce((p, v) => p + v.sector, 0)
