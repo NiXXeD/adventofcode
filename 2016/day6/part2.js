c0c5c46c434299655f82460b44533dc7c0065528
Andy Christianson <nix@nixxed.com>
2016-12-06 00:41:59 -0600

    Shorter again.

--- 2016/day6/part2.js
+++ 2016/day6/part2.js
@@ -1,2 +1,2 @@
 const _ = require('lodash')
-module.exports = i => _.zip(...i.map(v => v.split``)).map(v => _.minBy(v, c => v.filter(l => l == c).length)).join``
+module.exports = i => _.unzip(i.map(_.toArray)).map(v => _.minBy(v, c => v.filter(l => l == c).length)).join``
