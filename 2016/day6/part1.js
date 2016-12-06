684717dc83d5c998dcc628f005bbfd18b4e4f691
Andy Christianson <nix@nixxed.com>
2016-12-05 23:29:54 -0600

    Shorter day 6.

--- 2016/day6/part1.js
+++ 2016/day6/part1.js
@@ -1,17 +1,10 @@
 const _ = require('lodash')
 module.exports = input => {
-    return _.chain(input
-        .reduce((p, v) => {
-            v.split``.forEach((l, i) => {
-                if (!p[i][l]) p[i][l] = 0
-                p[i][l]++
-            })
-            return p
-        }, {0: {}, 1: {}, 2: {}, 3: {}, 4: {}, 5: {}, 6: {}, 7: {}}))
+    return _(input.reduce((p, v) => v.split``.forEach((c, i) => p[i] += c) || p, ['','','','','','','','']))
+        .map(v => _.countBy(v))
         .map(o => _.map(o, (v, k) => ({v, k})))
         .map(a => _.maxBy(a, 'v'))
         .map('k')
         .value()
         .join``
-
 }
