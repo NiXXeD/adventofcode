e44fdfa4059285dbbeedcf156cfb9be7d774fbe4
Andy Christianson <nix@nixxed.com>
2016-12-05 23:51:56 -0600

    Even shorter day 6.

--- 2016/day6/part1.js
+++ 2016/day6/part1.js
@@ -1,4 +1,2 @@
 const _ = require('lodash')
-module.exports = i => _.zip(...i.map(v => v.split``))
-    .map(v => _(_.countBy(v)).map((v, x) => ({v, x})).maxBy('v'))
-    .map(v => v.x).join``
+module.exports = i => _.zip(...i.map(v => v.split``)).map(v => _(_.countBy(v)).map((v, x) => ({v, x})).maxBy('v').x).join``
