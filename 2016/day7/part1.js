40bbb8bfba356ee790acf5de80156479b059e44e
Andy Christianson <nix@nixxed.com>
2016-12-07 01:40:20 -0600

    More correct part 1.

--- 2016/day7/part1.js
+++ 2016/day7/part1.js
@@ -1,1 +1,9 @@
-module.exports = i => i.filter(l => /([^[])([^[])\2\1/g.test(l) && /\[.*?(.)(.)\2\1.*?]/.test(l)).length
+module.exports = i => i.filter(line => {
+    let abba = [], habba = []
+    line.split(/\[|]/).forEach((section, x) => {
+        let res, rex = /(.)(.)\2\1/g
+        while ((res = rex.exec(section)) !== null && (rex.lastIndex -= 3))
+            if (res[0][0] !== res[0][1]) x % 2 ? habba.push(res[0]) : abba.push(res[0])
+    })
+    return abba.length > 0 && habba.length === 0
+}).length
--- 2016/day7/part2.js
+++ 2016/day7/part2.js
@@ -3,7 +3,7 @@ module.exports = i => i.filter(line => {
     line.split(/\[|]/).forEach((section, x) => {
         let res, rex = /(.)(.)\1/g
         while ((res = rex.exec(section)) !== null && (rex.lastIndex -= 2))
-            x % 2 ? bab.push(res[0]) : aba.push(res[0])
+            if (res[0][0] !== res[0][1]) x % 2 ? bab.push(res[0]) : aba.push(res[0])
     })
-    return aba.filter(a => a[0] !== a[1]).some(a => bab.includes(`${a[1]}${a[0]}${a[1]}`))
+    return aba.some(a => bab.includes(`${a[1]}${a[0]}${a[1]}`))
 }).length
