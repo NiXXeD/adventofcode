d18168c3486068bfc623c0bb42af348f1d9f514c
Andy Christianson <nix@nixxed.com>
2016-12-07 22:50:24 -0600

    Better part 1.

--- 2016/day7/part1.js
+++ 2016/day7/part1.js
@@ -1,9 +1,1 @@
-module.exports = i => i.filter(line => {
-    let abba = [], habba = []
-    line.split(/\[|]/).forEach((section, x) => {
-        let res, rex = /(.)(.)\2\1/g
-        while ((res = rex.exec(section)) !== null && (rex.lastIndex -= 3))
-            if (res[0][0] !== res[0][1]) x % 2 ? habba.push(res[0]) : abba.push(res[0])
-    })
-    return abba.length > 0 && habba.length === 0
-}).length
+module.exports = i => i.filter(l => /(\w)(?=(?!\1)(\w)\2\1)/g.test(l) && !/(\w)(?=(?!\1)(\w)\2\1\w*])/g.test(l)).length
