107243a563d2f04bf8866cad73eda379da633e57
Andy Christianson <nix@nixxed.com>
2016-12-10 15:10:53 -0600

    More concise day 10.

--- 2016/day10/part1.js
+++ 2016/day10/part1.js
@@ -1,33 +1,21 @@
 module.exports = (input, part = 1) => {
     let output = {}
-
     let bots = input
         .filter(value => /^bo/.test(value))
         .map(str => str.match(/(\d+).*?(output|bot) (\d+).*?(output|bot) (\d+)/))
         .map(matches => ({
             bot: +matches[1],
-            low: {
-                dest: matches[2],
-                value: +matches[3]
-            },
-            high: {
-                dest: matches[4],
-                value: +matches[5]
-            },
-            data: []
+            data: [],
+            low: chip => matches[2] === 'output' ? output[+matches[3]] = chip : bots[+matches[3]].process(chip),
+            high: chip => matches[4] === 'output' ? output[+matches[5]] = chip : bots[+matches[5]].process(chip)
         }))
         .map(bot => Object.assign(bot, {
             process(input) {
                 bot.data.push(input)
 
                 if (bot.data.length == 2) {
-                    let low = Math.min(...bot.data)
-                    let high = Math.max(...bot.data)
-
-                    bot.low.chip = low
-                    bot.high.chip = high
-                    bot.low.dest === 'output' ? output[bot.low.value] = low : bots[bot.low.value].process(low)
-                    bot.high.dest === 'output' ? output[bot.high.value] = high : bots[bot.high.value].process(high)
+                    bot.low(Math.min(...bot.data))
+                    bot.high(Math.max(...bot.data))
                 }
             }
         }))
@@ -35,16 +23,10 @@ module.exports = (input, part = 1) => {
 
     input.filter(value => /^va/.test(value))
         .map(str => str.match(/(\d+)/g))
-        .map(matches => ({
-            value: +matches[0],
-            bot: +matches[1]
-        }))
-        .forEach(value => {
-            bots[value.bot].process(value.value)
-        })
+        .forEach(matches => bots[+matches[1]].process(+matches[0]))
 
     if (part === 1) {
-        return bots.find(bot => bot.low.chip === 17 && bot.high.chip === 61).bot
+        return bots.find(bot => bot.data.includes(17) && bot.data.includes(61)).bot
     }
     return output[0] * output[1] * output[2]
 }
