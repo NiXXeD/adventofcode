468e0b6b89c92efe533e5c03c871ef988c9177e7
Andy Christianson <nix@nixxed.com>
2016-12-11 23:46:50 -0600

    Better.

--- 2016/day12/part1.js
+++ 2016/day12/part1.js
@@ -1,20 +1,18 @@
 module.exports = (input, part2 = false) => {
-    const deref = v => /a|b|c|d/.test(v) ? registers[v] : +v
+    let deref = v => 'abcd'.includes(v) ? registers[v] : +v
     let registers = {a: 0, b: 0, c: part2 ? 1 : 0, d: 0}
 
     for (let i = 0; i < input.length; i++) {
-        let cmd = input[i].split(' ')
-        let [a, b, c] = cmd
+        let [a, b, c] = input[i].split(' ')
 
-        if (a === 'cpy') {
+        if (a === 'cpy')
             registers[c] = deref(b)
-        } else if (a === 'inc') {
+        else if (a === 'inc')
             registers[b]++
-        } else if (a === 'dec') {
+        else if (a === 'dec')
             registers[b]--
-        } else if (a === 'jnz' && deref(b) !== 0) {
+        else if (a === 'jnz' && deref(b) !== 0)
             i += (+c) - 1
-        }
     }
 
     return registers.a
