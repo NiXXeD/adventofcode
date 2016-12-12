deeca50d7c75102afdbc61ae3aecfb05552937bb
Andy Christianson <nix@nixxed.com>
2016-12-12 00:14:42 -0600

    Better day 7 part 2.

--- 2016/day7/part2.js
+++ 2016/day7/part2.js
@@ -1,9 +1,2 @@
-module.exports = i => i.filter(line => {
-    let aba = [], bab = []
-    line.split(/\[|]/).forEach((section, x) => {
-        let res, rex = /(.)(.)\1/g
-        while ((res = rex.exec(section)) !== null && (rex.lastIndex -= 2))
-            if (res[0][0] !== res[0][1]) x % 2 ? bab.push(res[0]) : aba.push(res[0])
-    })
-    return aba.some(a => bab.includes(`${a[1]}${a[0]}${a[1]}`))
-}).length
+module.exports = i => i.filter(line => /(\w)(?!\1)(\w)\1(?=\w*\[).*\[\w*\2\1\2/.test(line) ||
+/(\w)(?!\1)(\w)\1(?=\w*]).*]\w*\2\1\2/.test(line)).length
