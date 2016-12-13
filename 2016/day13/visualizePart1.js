3815b6cb5d01ce577f50751e75bced4ce30c90f2
Andy Christianson <nix@nixxed.com>
2016-12-13 13:49:21 -0600

    Fix up visualizations a bit.

--- 2016/day13/visualizePart1.js
+++ 2016/day13/visualizePart1.js
@@ -4,80 +4,47 @@ const fs = require('fs')
 
 let input = fs.readFileSync(`./2016/day13/input`, 'utf-8').trimRight().split('\n')
 
-let fav = +input
+let goalX = 31, goalY = 39
 let test = (x, y) => {
-    let wat = (x * x) + (3 * x) + (2 * x * y) + y + (y * y) + fav
+    if (x > goalX + 10 || x < 0 || y < 0 || y > goalY + 10) return false
+    let wat = (x * x) + (3 * x) + (2 * x * y) + y + (y * y) + (+input)
     return wat.toString(2).split``.reduce((p, v) => +v + p, 0) % 2 === 0
 }
 
-function solve(goalX, goalY) {
-    let grid = []
-    for (let y = 0; y < goalY + 4; y++) {
-        grid[y] = []
-        for (let x = 0; x < goalX + 14; x++) {
-            grid[y][x] = test(x, y)
-        }
-    }
-
-    let x = 1, y = 1, moves = []
-    print()
-    while (!(x === goalX && y === goalY)) {
-        process.stdout.moveCursor(0, 0 - goalY - 5)
-        print()
-
-        if (_.get(grid, `[${y}][${x + 1}]`) && !_.find(moves, {x: x + 1, y})) {
-            x++
-        } else if (_.get(grid, `[${y + 1}][${x}]`) && !_.find(moves, {x, y: y + 1})) {
-            y++
-        } else if (_.get(grid, `[${y}][${x - 1}]`) && !_.find(moves, {x: x - 1, y})) {
-            x--
-        } else if (_.get(grid, `[${y - 1}][${x}]`) && !_.find(moves, {x, y: y - 1})) {
-            y--
-        } else {
-            grid[y][x] = false
-            moves.pop()
-            if (!moves.length) return -1
-            x = _.last(moves).x
-            y = _.last(moves).y
-            moves.pop()
-        }
-        moves.push({x, y})
-        checkPath()
-    }
-
-    process.stdout.moveCursor(0, 0 - goalY - 5)
-    print()
-
-    return moves.length
-
-    function checkPath() {
-        let three = _.nth(moves, -4)
-        let oops = three && (
-            (three.x + 1 === x && three.y === y) ||
-            (three.x - 1 === x && three.y === y) ||
-            (three.x === x && three.y + 1 === y) ||
-            (three.x === x && three.y - 1 === y)
-        )
-        if (oops) {
-            let a = moves.pop()
-            grid[a.y][a.x] = false
-            let b = moves.pop()
-            grid[b.y][b.x] = false
-        }
-    }
-
-    function print() {
-        let output = grid.map((row, gy) => {
-            return row.map((col, gx) => {
-                if (gy === y && gx === x) return 'X'
-                else if (gy === goalY && gx === goalX) return '@'
-                else if (_.find(moves, {x: gx, y: gy})) return 'O'
-                else return (col ? '.' : '#')
-            }).join``
-        }).join('\n')
-        console.log(output + `\n(${x}, ${y}) Moves: ${moves.length}        `)
-        sleep.usleep(1000000 / 25 >> 0)
+let grid = []
+for (let y = 0; y < goalY + 4; y++) {
+    grid[y] = []
+    for (let x = 0; x < goalX + 14; x++) {
+        grid[y][x] = test(x, y)
     }
 }
 
-solve(31, 39)
+let visited = []
+let moves = [{x: 1, y: 1, m: 0}]
+while (moves.length) {
+    let {x, y, m} = moves.pop()
+    visited.push({x, y, m})
+
+    if (_.find(visited, {x: goalX, y: goalY}))
+        break;
+    if (test(x + 1, y) && !_.find(visited, {X: x + 1, y}) && !_.find(moves, {X: x + 1, y}))
+        moves.unshift({x: x + 1, y, m: m + 1})
+    if (test(x, y + 1) && !_.find(visited, {x, y: y + 1}) && !_.find(moves, {x, y: y + 1}))
+        moves.unshift({x, y: y + 1, m: m + 1})
+    if (test(x - 1, y) && !_.find(visited, {x: x - 1, y}) && !_.find(moves, {x: x - 1, y}))
+        moves.unshift({x: x - 1, y, m: m + 1})
+    if (test(x, y - 1) && !_.find(visited, {x, y: y - 1}) && !_.find(moves, {x, y: y - 1}))
+        moves.unshift({x, y: y - 1, m: m + 1})
+
+    process.stdout.moveCursor(0, 0 - goalY - 7)
+    let output = grid.map((row, gy) => {
+        return row.map((col, gx) => {
+            if (_.find(visited, {x: gx, y: gy})) return 'O'
+            else return (col ? '.' : '#')
+        }).join``
+    }).join('\n')
+    console.log('\n' + output + `\nMoves: ${moves.length} Visited: ${visited.length}\n`)
+    sleep.usleep(1000000 / 25 >> 0)
+}
+
+console.log(`Final answer: ${visited.filter(v => v.x === goalX && v.y === goalY).slice(-1)[0].m}`)
--- 2016/day13/visualizePart2.js
+++ 2016/day13/visualizePart2.js
@@ -3,90 +3,44 @@ const sleep = require('sleep')
 const fs = require('fs')
 
 let input = fs.readFileSync(`./2016/day13/input`, 'utf-8').trimRight().split('\n')
-let fav = +input
 let test = (x, y) => {
-    let wat = (x * x) + (3 * x) + (2 * x * y) + y + (y * y) + fav
+    if (x < 0 || y < 0) return false
+    let wat = (x * x) + (3 * x) + (2 * x * y) + y + (y * y) + (+input)
     return wat.toString(2).split``.reduce((p, v) => +v + p, 0) % 2 === 0
 }
 
-function solve(maxSteps) {
-    let grid = []
-    for (let y = 0; y < 30; y++) {
-        grid[y] = []
-        for (let x = 0; x < 50; x++) {
-            grid[y][x] = test(x, y)
-        }
-    }
-
-    let x = 1, y = 1, moves = [], visited = [{x: 1, y: 1}], stuck = false
-    print()
-    while (!stuck) {
-        process.stdout.moveCursor(0, -31)
-        print()
-
-        let checkStuck = () => {
-            grid[y][x] = false
-            moves.pop()
-            if (!moves.length) {
-                stuck = true
-                return
-            }
-            x = _.last(moves).x
-            y = _.last(moves).y
-            moves.pop()
-        }
-        if (moves.length < maxSteps) {
-            if (_.get(grid, `[${y}][${x + 1}]`) && !_.find(moves, {x: x + 1, y})) {
-                x++
-            } else if (_.get(grid, `[${y + 1}][${x}]`) && !_.find(moves, {x, y: y + 1})) {
-                y++
-            } else if (_.get(grid, `[${y}][${x - 1}]`) && !_.find(moves, {x: x - 1, y})) {
-                x--
-            } else if (_.get(grid, `[${y - 1}][${x}]`) && !_.find(moves, {x, y: y - 1})) {
-                y--
-            } else {
-                checkStuck()
-            }
-        } else {
-            checkStuck()
-        }
-        moves.push({x, y})
-        if (!_.find(visited, {x, y})) visited.push({x, y})
-        checkPath()
-    }
-
-    process.stdout.moveCursor(0, -31)
-    print()
-    return visited.length
-
-    function checkPath() {
-        let three = _.nth(moves, -4)
-        let oops = three && (
-                (three.x + 1 === x && three.y === y) ||
-                (three.x - 1 === x && three.y === y) ||
-                (three.x === x && three.y + 1 === y) ||
-                (three.x === x && three.y - 1 === y)
-            )
-        if (oops) {
-            let a = moves.pop()
-            grid[a.y][a.x] = false
-            let b = moves.pop()
-            grid[b.y][b.x] = false
-        }
-    }
-
-    function print() {
-        let output = grid.map((row, gy) => {
-            return row.map((col, gx) => {
-                if (gy === y && gx === x) return 'X'
-                else if (gy === 1 && gx === 1) return 'O'
-                else if (_.find(visited, {x: gx, y: gy})) return '@'
-                else return (col ? '.' : '#')
-            }).join``
-        }).join('\n')
-        console.log(output + `\n(${x}, ${y}) Moves: ${moves.length}, Visited: ${visited.length}       `)
-        sleep.usleep(1000000 / 25 >> 0)
+let grid = []
+for (let y = 0; y < 30; y++) {
+    grid[y] = []
+    for (let x = 0; x < 50; x++) {
+        grid[y][x] = test(x, y)
     }
 }
 
-solve(50)
+let visited = []
+let moves = [{x: 1, y: 1, m: 0}]
+while (moves.length) {
+    let {x, y, m} = moves.pop()
+    visited.push({x, y})
+
+    if (m <= 51) {
+        if (test(x + 1, y) && !_.find(visited, {x: x + 1, y}))
+            moves.push({x: x + 1, y, m: m + 1})
+        if (test(x, y + 1) && !_.find(visited, {x, y: y + 1}))
+            moves.push({x, y: y + 1, m: m + 1})
+        if (test(x - 1, y) && !_.find(visited, {x: x - 1, y}))
+            moves.push({x: x - 1, y, m: m + 1})
+        if (test(x, y - 1) && !_.find(visited, {x, y: y - 1}))
+            moves.push({x, y: y - 1, m: m + 1})
+    }
+
+    process.stdout.moveCursor(0, -33)
+    let output = grid.map((row, gy) => {
+        return row.map((col, gx) => {
+            if (_.find(visited, {x: gx, y: gy})) return 'O'
+            else return (col ? '.' : '#')
+        }).join``
+    }).join('\n')
+    console.log('\n' + output + `\nVisited: ${visited.length}\n`)
+    sleep.usleep(1000000 / 25 >> 0)
+}
