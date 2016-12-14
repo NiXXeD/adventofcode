2e38796cf19ad51466b5c255bb86946124cbcb1e
Andy Christianson <nix@nixxed.com>
2016-12-13 20:12:20 -0600

    Shorter day 13.

--- 2016/day13/part1.js
+++ 2016/day13/part1.js
@@ -1,27 +1,17 @@
 module.exports = input => {
-    let goalX = 31, goalY = 39
-    let test = (x, y) => {
+    let goalX = 31, goalY = 39, visited = [], moves = [{x: 1, y: 1, m: 0}], test = (x, y, m) => {
         if (x > goalX + 10 || x < 0 || y < 0 || y > goalY + 10) return false
         let wat = (x * x) + (3 * x) + (2 * x * y) + y + (y * y) + (+input)
-        return wat.toString(2).split``.reduce((p, v) => +v + p, 0) % 2 === 0
+        let result = (wat.toString(2).split``.reduce((p, v) => +v + p, 0) % 2 === 0) &&
+            !visited.find(l => l.x === x && l.y === y) && !moves.find(l => l.x === x && l.y === y)
+        if (result) moves.unshift({x, y, m: m + 1})
     }
 
-    let visited = [], moves = [{x: 1, y: 1, m: 0}]
-    let find = (a, x, y) => a.find(l => l.x === x && l.y === y)
     while (moves.length) {
         let {x, y, m} = moves.pop()
         visited.push({x, y, m})
 
-        if (x === goalX && y === goalY) break;
-        if (test(x + 1, y) && !find(visited, x + 1, y) && !find(moves, x + 1, y))
-            moves.unshift({x: x + 1, y, m: m + 1})
-        if (test(x, y + 1) && !find(visited, x, y + 1) && !find(moves, x, y + 1))
-            moves.unshift({x, y: y + 1, m: m + 1})
-        if (test(x - 1, y) && !find(visited, x - 1, y) && !find(moves, x - 1, y))
-            moves.unshift({x: x - 1, y, m: m + 1})
-        if (test(x, y - 1) && !find(visited, x, y - 1) && !find(moves, x, y - 1))
-            moves.unshift({x, y: y - 1, m: m + 1})
+        if (x === goalX && y === goalY) return m
+        test(x + 1, y, m) || test(x, y + 1, m) || test(x - 1, y, m) || test(x, y - 1, m)
     }
-
-    return visited.filter(v => v.x === goalX && v.y === goalY).slice(-1)[0].m
 }
--- 2016/day13/part2.js
+++ 2016/day13/part2.js
@@ -1,27 +1,16 @@
 module.exports = input => {
-    let test = (x, y) => {
+    let visited = [], moves = [{x: 1, y: 1, m: 0}], test = (x, y, m) => {
         if (x < 0 || y < 0) return false
         let wat = (x * x) + (3 * x) + (2 * x * y) + y + (y * y) + (+input)
-        return wat.toString(2).split``.reduce((p, v) => +v + p, 0) % 2 === 0
+        let result = (wat.toString(2).split``.reduce((p, v) => +v + p, 0) % 2 === 0) &&
+            !visited.find(l => l.x === x && l.y === y) && !moves.find(l => l.x === x && l.y === y)
+        if (result && m < 50) moves.unshift({x, y, m: m + 1})
     }
 
-    let visited = []
-    let moves = [{x: 1, y: 1, m: 0}]
-    let find = (a, x, y) => a.find(l => l.x === x && l.y === y)
     while (moves.length) {
         let {x, y, m} = moves.pop()
         visited.push({x, y})
-
-        if (m < 50) {
-            if (test(x + 1, y) && !find(visited, x + 1, y) && !find(moves, x + 1, y))
-                moves.unshift({x: x + 1, y, m: m + 1})
-            if (test(x, y + 1) && !find(visited, x, y + 1) && !find(moves, x, y + 1))
-                moves.unshift({x, y: y + 1, m: m + 1})
-            if (test(x - 1, y) && !find(visited, x - 1, y) && !find(moves, x - 1, y))
-                moves.unshift({x: x - 1, y, m: m + 1})
-            if (test(x, y - 1) && !find(visited, x, y - 1) && !find(moves, x, y - 1))
-                moves.unshift({x, y: y - 1, m: m + 1})
-        }
+        test(x + 1, y, m) || test(x, y + 1, m) || test(x - 1, y, m) || test(x, y - 1, m)
     }
 
     return visited.length
