18b4e903ff5f13b20ba76f60c8b364e05547173f
Andy Christianson <nix@nixxed.com>
2016-12-15 01:02:04 -0600

    Better day 15.

--- 2016/day15/part1.js
+++ 2016/day15/part1.js
@@ -1,25 +1,7 @@
 module.exports = (input, part2 = false) => {
-    let discs = input.map(str => str.match(/(\d+)/g))
-        .map(([disc, pos, time, cur]) => {
-            return {
-                disc: +disc,
-                pos: +pos,
-                cur: +cur,
-                ready: ((+pos - +disc) + +pos) % +pos
-            }
-        })
-
-    if (part2) discs.push({disc: 7, pos: 11, cur: 0, ready: 4})
-
-    let time = 0
-    let found = false
-    while (!found) {
-        time++
-        discs.map(disc => {
-            disc.cur = (disc.cur + 1) % disc.pos
-        })
-        found = discs.every(disc => disc.ready === disc.cur)
-    }
-
+    let time = 0, discs = input.map(str => str.match(/(\d+)/g))
+        .map(([disc, pos,, cur]) => ({pos: +pos, cur: +cur, ready: (2 * +pos - +disc) % +pos}))
+        .concat(part2 ? [{pos: 11, cur: 0, ready: 4}] : [])
+    while (!discs.every(disc => disc.ready === ((disc.cur + time) % disc.pos))) time++
     return time
 }
