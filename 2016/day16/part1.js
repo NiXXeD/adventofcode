4b36e4b2b0ee60f310f71a567dde3c682fc1c9ba
Andy Christianson <nix@nixxed.com>
2016-12-17 15:17:17 -0600

    Faster rather than shorter.

--- 2016/day16/part1.js
+++ 2016/day16/part1.js
@@ -1,5 +1,16 @@
-module.exports = (i, s = 272) => {
-    for (var d = i; d.length < s; d = `${d}0${d.split``.reverse().map(c => (+c + 1) % 2).join``}`);
-    for (d = d.slice(0, s); !(d.length % 2); d = d.match(/../g).map(s => s[0] == s[1] ? '1' : '0').join``);
-    return d
+module.exports = (input, size = 272) => {
+    let data = input.split``.map(i => +i)
+    do {
+        data = data.concat(0, data.slice().reverse().map(c => (+c + 1) % 2))
+    } while (data.length < size)
+    data = data.slice(0, size)
+
+    do {
+        let ndata = []
+        for (let i = 0; i < data.length; i += 2)
+            ndata.push(data[i] === data[i + 1] ? 1 : 0)
+        data = ndata
+    } while (!(data.length % 2))
+
+    return data.join``
 }
