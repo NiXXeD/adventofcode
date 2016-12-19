4e8a7f4342dd334456a22be7b1a3d3e6a47f8a5a
Andy Christianson <nix@nixxed.com>
2016-12-19 02:09:40 -0600

    Shorter part 2.

--- 2016/day19/part2.js
+++ 2016/day19/part2.js
@@ -1,10 +1,8 @@
 module.exports = input => {
-    let target = +input
-    let powers = [...Array(20).keys()].map(i => Math.pow(3, i)).reverse()
-    let highestPower = powers.find(i => i <= target)
-    let diff = target - highestPower
+    let target = +input, powers = [...Array(20).keys()].map(i => Math.pow(3, i)).reverse(),
+        highestPower = powers.find(i => i <= target)
 
     if (highestPower === target) return highestPower
-    else if (highestPower >= target / 2) return diff
-    else return highestPower + (2 * (diff - highestPower))
+    else if (highestPower >= target / 2) return target - highestPower
+    else return highestPower + (2 * (target - 2 * highestPower))
 }
