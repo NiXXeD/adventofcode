173aa55e8d9cad47e95c7dcae82314b015500536
Andy Christianson <nix@nixxed.com>
2016-12-19 17:00:59 -0600

    Shorter else

--- 2016/day19/part2.js
+++ 2016/day19/part2.js
@@ -2,5 +2,5 @@ module.exports = input => {
     let target = +input, pow = Math.pow(3, target.toString(3).length - 1)
     if (pow === target) return pow
     else if (pow >= target / 2) return target - pow
-    else return pow + (2 * (target - 2 * pow))
+    else return 2 * target - 3 * pow
 }
