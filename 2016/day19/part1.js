9ef08f6380780d5e70134f44e2c5555723cd8309
Andy Christianson <nix@nixxed.com>
2016-12-19 01:37:32 -0600

    Better part 1.

--- 2016/day19/part1.js
+++ 2016/day19/part1.js
@@ -1,8 +1,1 @@
-const _ = require('lodash')
-module.exports = input => {
-    let target = +input
-    let powers = _.range(24).map(i => Math.pow(2, i))
-    let highestPower = _.findLast(powers, i => i <= target)
-    let diff = target - highestPower
-    return 2 * diff + 1
-}
+module.exports = i => parseInt((+i).toString(2).slice(1) + (+i).toString(2).slice(0, 1), 2)
