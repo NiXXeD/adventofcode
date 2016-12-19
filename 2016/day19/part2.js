72206319aefa06d527415336bbbf6a96b39d3021
Andy Christianson <nix@nixxed.com>
2016-12-19 11:04:49 -0600

    Easier power check.

--- 2016/day19/part2.js
+++ 2016/day19/part2.js
@@ -1,8 +1,6 @@
 module.exports = input => {
-    let target = +input, powers = [...Array(20).keys()].map(i => Math.pow(3, i)).reverse(),
-        highestPower = powers.find(i => i <= target)
-
-    if (highestPower === target) return highestPower
-    else if (highestPower >= target / 2) return target - highestPower
-    else return highestPower + (2 * (target - 2 * highestPower))
+    let target = +input, pow = Math.pow(3, target.toString(3).length - 1)
+    if (pow === target) return pow
+    else if (pow >= target / 2) return target - pow
+    else return pow + (2 * (target - 2 * pow))
 }
