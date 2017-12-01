61066fc5ffae936db4b80ace661cb4aeee94ab36
Andy Christianson <nix@nixxed.com>
2017-12-01 10:48:58 -0600

    Shorter day 1 part 1.

--- 2017/day1/part1.js
+++ 2017/day1/part1.js
@@ -1,3 +1,3 @@
-module.exports = input => input.match(/(.)\1+/g)
+module.exports = input => (input + input[0]).match(/(.)\1+/g)
     .map(m => +m[0] * (m.length - 1))
-    .reduce((acc, val) => acc + val) + (input[0] === input.slice(-1) ? +input[0] : 0)
+    .reduce((acc, val) => acc + val)
