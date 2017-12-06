de16a71fed52bf9b44e2e030ad6a7f7533bd5618
Andy Christianson <nix@nixxed.com>
2017-12-06 14:07:54 -0600

    Slightly shorter day6, and combined parts.

--- 2017/day6/part1.js
+++ 2017/day6/part1.js
@@ -1,23 +1,21 @@
-module.exports = input => {
+module.exports = (input, part2 = false) => {
     let banks = input.split(/\s/).map(i => +i)
     let cache = {}, next = '', cycles = 0
 
     while (!cache.hasOwnProperty(next)) {
-        cache[next] = cycles
-        cycles++
+        cache[next] = cycles++
 
         let value = banks.reduce((a, v) => Math.max(a, v), 0)
         let index = banks.findIndex(v => v === value)
         banks[index] = 0
 
-        while (value) {
+        while (value--) {
             index = (index + 1) % banks.length
             banks[index]++
-            value--
         }
 
         next = banks.reduce((a, v) => a + v, '')
     }
 
-    return cycles
+    return part2 ? cycles - cache[next] : cycles
 }
