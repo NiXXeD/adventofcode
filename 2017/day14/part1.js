da5fb5ef0e0b0c44ab07f56c9067f950d2fd4761
Andy Christianson <nix@nixxed.com>
2017-12-14 14:15:15 -0600

    Shorter Day 14 Part 1.

--- 2017/day14/part1.js
+++ 2017/day14/part1.js
@@ -1,3 +1,2 @@
 module.exports = input => [...Array(128)].map((k, i) => require('../day10/part2')(`${input}-${i}`))
-    .map(hash => hash.split``.map(hex => parseInt(hex, 16).toString(2).padStart(4, '0').replace(/0/g, '')).join``)
-    .join``.length
+    .map(hash => hash.split``.map(hex => parseInt(hex, 16).toString(2).replace(/0/g, '')).join``).join``.length
