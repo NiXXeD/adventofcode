352faf1df6ade4291a7319d377ca730b28ef097a
Andy Christianson <nix@nixxed.com>
2017-12-16 23:32:50 -0600

    Less vars.

--- 2017/day17/part1.js
+++ 2017/day17/part1.js
@@ -1,9 +1,9 @@
 module.exports = input => {
-    let steps = +input, buffer = [0], index = 0, current = 1
+    let steps = +input, buffer = [0], index = 0
 
     for (let i = 0; i < 2017; i++) {
         index = (index + steps) % buffer.length + 1
-        buffer.splice(index, 0, current++)
+        buffer.splice(index, 0, i + 1)
     }
 
     return buffer[index + 1]
--- 2017/day17/part2.js
+++ 2017/day17/part2.js
@@ -1,10 +1,9 @@
 module.exports = input => {
-    let steps = +input, index = 0, current = 1, valAfterZero = 1
+    let steps = +input, index = 0, valAfterZero = 1
 
     for (let i = 0; i < 50000000; i++) {
-        index = (index + steps) % current + 1
-        if (index === 1) valAfterZero = current
-        current++
+        index = (index + steps) % (i + 1) + 1
+        if (index === 1) valAfterZero = i + 1
     }
 
     return valAfterZero
