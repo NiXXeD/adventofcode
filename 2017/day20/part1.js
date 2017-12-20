e7a9adda27e6d0fc40f9882af07a4dbef4637e00
Andy Christianson <nix@nixxed.com>
2017-12-20 17:07:29 -0600

    Faster, shorter, day 20.

--- 2017/day20/part1.js
+++ 2017/day20/part1.js
@@ -1,21 +1,20 @@
 module.exports = input => {
     let particles = input.map(str => str.match(/([\d-]+)/g).map(i => +i))
         .map(([px, py, pz, vx, vy, vz, ax, ay, az], i) => ({i, pos: [px, py, pz], vel: [vx, vy, vz], acc: [ax, ay, az]}))
-    const distance = point => point.distance = point.pos.reduce((a, v) => a + Math.abs(v), 0)
+    const distance = p => p.pos.reduce((a, v) => a + Math.abs(v), 0)
     const apply = (([x, y, z], [dx, dy, dz]) => ([x + dx, y + dy, z + dz]))
-    const accelerate = point => point.vel = apply(point.vel, point.acc)
-    const move = point => point.pos = apply(point.pos, point.vel)
+    const accelerate = p => p.vel = apply(p.vel, p.acc)
+    const move = p => p.pos = apply(p.pos, p.vel)
 
     for (let i = 0; i < 500; i++) {
         particles.forEach(particle => {
             accelerate(particle)
             move(particle)
-            distance(particle)
         })
     }
 
     return particles.reduce((acc, val) => {
-        if (val.distance < acc.distance) return val
+        if (distance(val) < distance(acc)) return val
         else return acc
     }, particles[0]).i
 }
--- 2017/day20/part2.js
+++ 2017/day20/part2.js
@@ -2,8 +2,9 @@ module.exports = input => {
     let particles = input.map(str => str.match(/([\d-]+)/g).map(i => +i))
         .map(([px, py, pz, vx, vy, vz, ax, ay, az]) => ({pos: [px, py, pz], vel: [vx, vy, vz], acc: [ax, ay, az]}))
     const apply = (([x, y, z], [dx, dy, dz]) => ([x + dx, y + dy, z + dz]))
-    const accelerate = point => point.vel = apply(point.vel, point.acc)
-    const move = point => point.pos = apply(point.pos, point.vel)
+    const accelerate = p => p.vel = apply(p.vel, p.acc)
+    const move = p => p.pos = apply(p.pos, p.vel)
+    const key = p => `${p.pos[0]},${p.pos[1]},${p.pos[2]}`
 
     let noCollideCount = 0
     while (noCollideCount < 10) {
@@ -13,12 +14,12 @@ module.exports = input => {
         })
 
         let lastCount = particles.length
-        particles = particles
-            .filter(a => particles
-                .filter(b =>
-                    a.pos[0] === b.pos[0] && a.pos[1] === b.pos[1] && a.pos[2] === b.pos[2]
-                ).length === 1
-            )
+        let counts = particles.reduce((acc, particle) => {
+            let id = key(particle)
+            acc[id] = (acc[id] || 0) + 1
+            return acc
+        }, {})
+        particles = particles.filter(particle => counts[key(particle)] === 1)
         if (lastCount === particles.length) noCollideCount++
         else noCollideCount = 0
     }
