4f4fec5590a7703508813bfc493c4c2185203efe
Andy Christianson <nix@nixxed.com>
2017-12-20 17:24:58 -0600

    Shorter day 19.

--- 2017/day19/part2.js
+++ 2017/day19/part2.js
@@ -1,7 +1,6 @@
 module.exports = input => {
-    let map = input.map(str => str.split``), dx = 0, dy = 1, y = 0, x = map[0].indexOf('|')
-
-    const get = (x, y) => (map[y] && map[y][x]) || ''
+    let dx = 0, dy = 1, y = 0, x = input[0].indexOf('|')
+    const get = (x, y) => (input[y] && input[y][x]) || ''
     const getNext = () => {
         x += dx
         y += dy
@@ -13,19 +12,10 @@ module.exports = input => {
     while ((next = getNext()) !== '') {
         steps++
         if (next === '+') {
-            if (dy !== 1 && test(x, y - 1)) {
-                dx = 0
-                dy = -1
-            } else if (dy !== -1 && test(x, y + 1)) {
-                dx = 0
-                dy = 1
-            } else if (dx !== -1 && test(x + 1, y)) {
-                dx = 1
-                dy = 0
-            } else if (dx !== 1 && test(x - 1, y)) {
-                dx = -1
-                dy = 0
-            }
+            if (dy !== 1 && test(x, y - 1)) [dx, dy] = [0, -1]
+            else if (dy !== -1 && test(x, y + 1)) [dx, dy] = [0, 1]
+            else if (dx !== -1 && test(x + 1, y)) [dx, dy] = [1, 0]
+            else if (dx !== 1 && test(x - 1, y)) [dx, dy] = [-1, 0]
         }
     }
 
