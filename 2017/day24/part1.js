48c5499b2529414b58cd1c0d7b4907b3cf9a28aa
Andy Christianson <nix@nixxed.com>
2017-12-27 15:39:45 -0600

    Faster day 24.

--- 2017/day24/part1.js
+++ 2017/day24/part1.js
@@ -2,14 +2,14 @@ module.exports = input => {
     let components = input.map(str => str.split`/`)
         .map(([a, b]) => ({id: `${a}/${b}`, a: +a, b: +b}))
 
-    let queue = [{port: 0, used: [], strength: 0}], best = 0
+    let queue = [{port: 0, used: '', strength: 0}], best = 0
     do {
         let {port, used, strength} = queue.pop()
         let valid = components.filter(comp => (comp.a === port || comp.b === port) && !used.includes(comp.id))
         if (valid.length) {
             queue.push(...valid.map(comp => ({
                 port: (comp.a === port ? comp.b : comp.a),
-                used: [...used, comp.id],
+                used: `${used},${comp.id}`,
                 strength: strength + comp.a + comp.b
             })))
         } else {
